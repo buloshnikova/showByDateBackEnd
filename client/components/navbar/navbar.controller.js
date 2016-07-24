74a591cb9c67979de2053935180142816b00928c
Inna Ziskind <innaz@nuro.im>
2016-07-24 19:57:25 +0200

    Initial commit

---
+++ client/components/navbar/navbar.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+class NavbarController {}
+
+angular.module('showByDateApp')
+  .controller('NavbarController', NavbarController);
---
+++ server/config/environment/development.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://pashaweb:pashak666@ds015325.mlab.com:15325/showbydate'
+  },
+
+  // Seed database on startup
+  seedDB: true
+
+};
