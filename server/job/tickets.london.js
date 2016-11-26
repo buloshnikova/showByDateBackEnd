b91ddf9b1bedb1958eb52b6625ab3b62d566d190
pavel.kogan <pavel.kogan@sizmek.com>
2016-11-26 15:11:41 +0200

    adding gem

--- server/job/tickets.london.js
+++ server/job/tickets.london.js
@@ -12,7 +12,25 @@ var saveToDB = require('./util/saveToDB');
 var moment = require('moment-timezone');
 var page = 0;
 var resp;
-
+const types = [
+  {
+    eventType: 'SportEvent',
+    performerType: 'sportPerformer',
+    link: 'http://tickets.london/search?browseorder=soonest&dend=26%2F12%2F2016&distance=0&availableonly=False&showfavourites=True&se=False&s=sport&pageSize=30&pageIndex='
+  },
+  {
+    eventType: 'TheatreEvent',
+    performerType: 'TheatrePerformer',
+    link: 'http://tickets.london/search?browseorder=soonest&dend=26%2F12%2F2016&distance=0&availableonly=False&showfavourites=True&se=False&s=theatre&pageSize=30&pageIndex='
+  },
+  {
+    eventType: 'MusicEvent',
+    performerType: 'MusicPerformer',
+    link: 'http://tickets.london/search?browseorder=soonest&dend=26%2F12%2F2016&distance=0&availableonly=False&showfavourites=True&se=False&s=music&pageSize=30&pageIndex='
+  }
+]
+var index = 0
+var currentType = types[0];
 // Define the scrape function
 function respondWithResult(res, statusCode) {
   statusCode = statusCode || 200;
@@ -22,14 +40,12 @@ function respondWithResult(res, statusCode) {
     }
   };
 }
-
 function handleError(res, statusCode) {
   statusCode = statusCode || 500;
   return function (err) {
     res.status(statusCode).send(err);
   };
 }
-
 function scrape(url, cb) {
   // 1. Create the request
   req(url, (err, body) => {
@@ -59,7 +75,7 @@ function scrape(url, cb) {
           let date = moment(datePre[1] + ' ' + p[1].children[0].data, 'YYYY dddd Do MMMM at h:mm A').format('x');
           if (!isNaN(date)) {
             let obj = {
-              '@type': 'SportEvent',
+              '@type': currentType.eventType,
               name: el('h3 a').text(),
               url: sitePrefix + el('h3 a').attr("href"),
               location: {
@@ -68,7 +84,7 @@ function scrape(url, cb) {
               },
               startDate: date,
               performer: {
-                '@type': 'sportPerformer',
+                '@type': currentType.performerType,
                 'name': sitePrefix + el('h3 a').text(),
                 'sameAs': sitePrefix + el('h3 a').attr("href")
               },
@@ -116,27 +132,41 @@ function getWebSiteID(res) {
 export function job(req, res) {
   console.log("Started");
   page = 0;
+  index = 0;
+  currentType = types[0];
   getWebSiteID();
   resp = res;
   return res.status(200).json({message: "process started"})
 }
-
+function goToNextCategory(){
+  index = index + 1;
+  currentType = types[index];
+  page = 0;
+  getHtmlPage();
+}
 function getHtmlPage() {
   page++;
-  console.log("Get Page:", page);
-  scrape("http://tickets.london/search?browseorder=soonest&distance=0&availableonly=False&showfavourites=True&se=False&s=sport&pageSize=30&c=18&pageIndex=" + page
+  //console.log("Get Page:", page);
+  //console.log('Page:', currentType.link + page.toString());
+  scrape(currentType.link + page.toString()
     , (err, data) => {
+
       if (err) {
+        console.log(index);
+        if (index < 2) {
+          goToNextCategory()
+        } else {
+          console.log("End of process");
+          return;
+        }
         //process.exit();
-        console.log("End of process");
-        return;
+
       } else {
         /// parceResult(data);
         saveToDB.save(data, webSiteID, getHtmlPage);
       }
     });
 }
-
 
 function checkWebsiteInDB() {
 
