3ba366b537c8f5ccbb7d1196f07ff13ec46cbda4
pashaweb <pashaweb@gmail.com>
2017-05-21 11:26:27 +0300

    stub hub

--- server/job/stub-hub.js
+++ server/job/stub-hub.js
@@ -8,127 +8,162 @@ let venues = new Map();
 let events = [];
 let webSiteID = "";
 let websiteUrl = "https://www.stubhub.com/";
+let collected = [];
 const args = {
-    headers: {
-        'Authorization': 'Bearer d9814d83-4b0c-335b-8e0d-f0308d1514e9'
-    }
+  headers: {
+    'Authorization': 'Bearer d9814d83-4b0c-335b-8e0d-f0308d1514e9'
+  }
 };
 let config = [
-    { url: 'https://api.stubhub.com/search/catalog/events/v3?city=London&country=gb&start=0&rows=500&q=Sport', eventType: 'SportEvent', performerType: 'SportPerformer' },
-    { url: 'https://api.stubhub.com/search/catalog/events/v3?city=London&country=gb&start=0&rows=500&q=Concert', eventType: 'MusicEvent', performerType: 'MusicPerformer' },
-    { url: 'https://api.stubhub.com/search/catalog/events/v3?city=London&country=gb&start=0&rows=500&q=Theater', eventType: 'TheaterEvent', performerType: 'TheaterPerformer' }
+  {
+    url: 'https://api.stubhub.com/search/catalog/events/v3?city=London&country=gb&q=Sport',
+    eventType: 'SportEvent',
+    performerType: 'SportPerformer'
+  },
+  {
+    url: 'https://api.stubhub.com/search/catalog/events/v3?city=London&country=gb&q=Concert',
+    eventType: 'MusicEvent',
+    performerType: 'MusicPerformer'
+  },
+  {
+    url: 'https://api.stubhub.com/search/catalog/events/v3?city=London&country=gb&q=Theater',
+    eventType: 'TheaterEvent',
+    performerType: 'TheaterPerformer'
+  }
 ]
 
 let currentConfigIndex = 0;
-let page = 0;
+let start = 0;
 let limit = 500;
 
 export function job(req, res) {
-    getWebSiteID();
-    return res.status(200).json({ message: "process started" })
+  getWebSiteID();
+  return res.status(200).json({message: "process started"})
 }
 
 function getWebSiteID(res) {
-    website.findOne({ name: webSiteName })
-        .then(function(response) {
-            if (response !== null) {
-                webSiteID = response._id;
-                //parceResult(data, res);
-                getEvents();
-            } else {
-                website.create({
-                    name: webSiteName,
-                    websiteUrl: websiteUrl,
-                    rating: 5,
-                    logoUrl: "",
-                    defaultImageUrl: "",
-                    active: true
-                }).then(function(response) {
-                    webSiteID = response._id;
-                    getEvents();
-                });
-            }
+  website.findOne({name: webSiteName})
+    .then(function (response) {
+      if (response !== null) {
+        webSiteID = response._id;
+        //parceResult(data, res);
+        getEvents();
+      } else {
+        website.create({
+          name: webSiteName,
+          websiteUrl: websiteUrl,
+          rating: 5,
+          logoUrl: "",
+          defaultImageUrl: "",
+          active: true
+        }).then(function (response) {
+          webSiteID = response._id;
+          getEvents();
         });
+      }
+    });
 }
 
 function getEvents() {
-    debugger
-    client.get(config[currentConfigIndex].url, args,
-        function(data, response) {
+console.log('getEvents: ' + '&start=' + start + '&rows=' + limit)
+  let sufix = '&start=' + start + '&rows=' + limit;
+  client.get(config[currentConfigIndex].url + sufix, args,
+    function (data, response) {
 
-            //console.log(data)
-            events = data.events.map(ev => {
-                //let venue = venues.get(ev.VenueId);
-                let eventName = ev.name;
-                let venue = {
-                    "type": "Place",
-                    "address": {
-                        "type": "PostalAddress",
-                        "addressLocality": ev.venue.address1,
-                        "streetAddress": "London"
-                    },
-                    "name": ev.venue.name
-                }
+      events = data.events.map(ev => {
+        //let venue = venues.get(ev.VenueId);
+        let eventName = ev.name;
+        let venue = {
+          "type": "Place",
+          "address": {
+            "type": "PostalAddress",
+            "addressLocality": ev.venue.address1,
+            "streetAddress": "London"
+          },
+          "name": ev.venue.name
+        }
 
-                let performers = [];
-                if (ev.performers) {
-                    performers = ev.performers.map(performer => {
-                        return {
-                            '@type': config[currentConfigIndex].performerType,
-                            'name': performer.name
-                        }
-                    })
-                } else {
-                    performers[0] = {
-                        '@type': config[currentConfigIndex].performerType,
-                        'name': ev.name
-                    };
-                }
-
-
-                return {
-                    '@type': config[currentConfigIndex].eventType,
-                    name: eventName,
-                    url: websiteUrl + ev.webURI,
-                    location: venue,
-                    startDate: moment.tz(ev.eventDateUTC, "Europe/London").format('x'), ///el('.event-meta strong').attr('content'),
-                    endDate: moment.tz(ev.eventDateUTC, "Europe/London").format('x'),
-                    performer: performers,
-                    price: '',
-                    eventImage: ev.imageUrl,
-                    active: false,
-                    website: webSiteID
-                }
-            });
-            // if (config[currentConfigIndex].eventType == 'TheaterEvent') {
-            //     var myMap = new Map();
-            //     for (let i = 0; i < events.length; i++) {
-            //         let name = events[i].name;
-            //         let tempFromMap = myMap.get(name);
-            //         if (tempFromMap == null) {
-            //             myMap.set(name, events[i])
-            //         } else {
-            //             tempFromMap.endDate = events[i].endDate;
-            //             myMap.set(name, tempFromMap);
-
-            //         }
-            //     }
-            //     events = []
-            //     myMap.forEach(function(value, key) {
-            //         events.push(value)
-            //     });
-            // }
-            console.log(events)
-                //saveToDB.save(events, webSiteID, endProcess);
-            currentConfigIndex += 1;
-            if (currentConfigIndex < config.length) {
-                getEvents();
+        let performers = [];
+        if (ev.performers) {
+          performers = ev.performers.map(performer => {
+            return {
+              '@type': config[currentConfigIndex].performerType,
+              'name': performer.name
             }
-        });
+          })
+        } else {
+          performers[0] = {
+            '@type': config[currentConfigIndex].performerType,
+            'name': ev.name
+          };
+        }
+
+        let uri = ( config[currentConfigIndex].performerType == 'TheaterPerformer' && ev.groupings && ev.groupings.length > 1 && ev.groupings[1].webURI != null) ? ev.groupings[1].webURI : ev.webURI;
+        return {
+          '@type': config[currentConfigIndex].eventType,
+          name: eventName,
+          url: websiteUrl + uri,
+          location: venue,
+          startDate: moment.tz(ev.eventDateUTC, "Europe/London").format('x'), ///el('.event-meta strong').attr('content'),
+          endDate: moment.tz(ev.eventDateUTC, "Europe/London").format('x'),
+          performer: performers,
+          price: '',
+          eventImage: ev.imageUrl,
+          active: false,
+          website: webSiteID
+        }
+      });
+
+      collected = collected.concat(events);
+      if (collected.length < data.numFound) {
+        start = collected.length;
+        getEvents();
+      } else {
+        console.log('colected to save',collected.length);
+        //goToNexCategory();
+        if (config[currentConfigIndex].eventType != 'TheaterEvent') {
+          saveToDB.save(collected, webSiteID, goToNexCategory())
+        } else {
+          let arrToSave = theaterReduse();
+          saveToDB.save(arrToSave, webSiteID, goToNexCategory())
+        }
+
+      }
+
+    });
+}
+
+function groupBy(xs, key) {
+  return xs.reduce(function (rv, x) {
+    (rv[x[key]] = rv[x[key]] || []).push(x);
+    return rv;
+  }, {});
+};
+
+function theaterReduse() {
+  let temp = groupBy(collected, 'name');
+  let ret = [];
+  for (let i in temp) {
+    console.log(i, ':', temp[i].length);
+    let tempEvent = temp[i][0];
+    tempEvent.endDate = temp[i][temp[i].length - 1].endDate;
+    ret.push(tempEvent);
+  }
+  return ret
+
+}
+function goToNexCategory() {
+  console.log("goToNexCategory");
+  collected = [];
+  start=0;
+  currentConfigIndex += 1;
+  if (currentConfigIndex < config.length) {
+    getEvents();
+  }
 }
 
 
 function endProcess() {
-    currentConfigIndex = 0;
-    console.log('End Process')
+  currentConfigIndex = 0;
+  console.log('End Process')
 }
--- server/job/util/saveToDB.js
+++ server/job/util/saveToDB.js
@@ -5,99 +5,107 @@ var eventTypeToID = require('./eventTypeToID');
 var performerToID = require('./performerToID');
 var async = require('async');
 export function save(arr, websiteID, cb) {
-    let out = [];
-    async.eachSeries(arr,
-        function(item, next) {
-            let outItem = {};
-            async.waterfall([
-                function(callback) {
-                    let tempName = item.name.toLowerCase();
-                    let query = item['@type'] == 'TheaterEvent' ? { name_lower: tempName } : { name_lower: tempName, startDate: item.startDate, endDate: item.endDate }
-                    show_by_date_event.findOne({ name_lower: tempName, startDate: item.startDate, endDate: item.endDate })
-                        .then(function(response) {
-                            if (response !== null) {
-                                callback(true);
-                            } else {
-                                callback(null);
-                            }
-                        });
-                },
-                function(callback) {
-                    let tempName = item.name.toLowerCase();
-                    Favorite.findOne({ name_lower: tempName })
-                        .then(function(res) {
-                            if (res != null) {
-                                outItem.favorite = true;
-                            } else {
-                                outItem.favorite = false;
-                            }
-                            callback(null);
-                        });
-                },
-                function(callback) {
-                    //console.log("not exist");
-                    //get propretis ids
-                    async.parallel({
-                        eventTypeID: function(callback) {
-                            eventTypeToID.getID(item['@type'], callback);
-                        },
-                        location: function(callback) {
-                            locationToID.getID(item.location, callback);
-                        },
-                        performers: function(callback) {
-                            performerToID.getID(item.performer, callback);
-                        }
-                    }, function(err, results) {
-                        // results is now equals to: {one: 1, two: 2}
-                        //console.log("results", results);
-                        outItem.eventType = results.eventTypeID;
-                        if (results.performers.hasFavorites) {
-                            outItem.favorite = true;
-                        }
-                        outItem.location = results.location;
-                        outItem.performer = results.performers.idsList;
-                        out.push(outItem);
-                        callback(null)
-                    });
-                    // arg1 now equals 'one' and arg2 now equals 'two'
-                    //callback(null, 'three');
-                },
-                function(callback) {
-
-                    outItem.name = item.name;
-                    outItem.name_lower = item.name.toLowerCase();
-                    outItem.url = item.url;
-                    outItem.startDate = item.startDate;
-                    outItem.endDate = item.endDate;
-                    outItem.website = websiteID;
-                    outItem.price = null;
-                    outItem.eventImage = item.eventImage;
-                    outItem.active = true;
-                    console.log("Create: ", outItem.name);
-
-                    show_by_date_event.create(outItem)
-                        .then(function(res) {
-                            callback(null, 'done');
-                        }, function(err) {
-                            console.log('error:', err);
-                            callback(null, 'done');
-                            // handle error here.
-                        })
-                        // arg1 now equals 'three')
-
-                }
-            ], function(err, result) {
-                if (err) {
-                    next();
-                } else {
-                    out.push(outItem);
-                    next();
-                }
+  let out = [];
+  async.eachSeries(arr,
+    function (item, next) {
+      let outItem = {};
+      async.waterfall([
+        function (callback) {
+          let tempName = item.name.toLowerCase();
+          let query = item['@type'] == 'TheaterEvent' ? {name_lower: tempName} : {
+            name_lower: tempName,
+            startDate: item.startDate,
+            endDate: item.endDate
+          }
+          show_by_date_event.findOne({name_lower: tempName, startDate: item.startDate, endDate: item.endDate})
+            .then(function (response) {
+              if (response !== null) {
+                callback(true);
+              } else {
+                callback(null);
+              }
             });
         },
-        function(err) {
-            // console.log(out);
-            //return res.status(200).json(out)
-            cb();
-        })
+        function (callback) {
+          let tempName = item.name.toLowerCase();
+          Favorite.findOne({name_lower: tempName})
+            .then(function (res) {
+              if (res != null) {
+                outItem.favorite = true;
+              } else {
+                outItem.favorite = false;
+              }
+              callback(null);
+            });
+        },
+        function (callback) {
+          //console.log("not exist");
+          //get propretis ids
+          async.parallel({
+            eventTypeID: function (callback) {
+              eventTypeToID.getID(item['@type'], callback);
+            },
+            location: function (callback) {
+              locationToID.getID(item.location, callback);
+            },
+            performers: function (callback) {
+              performerToID.getID(item.performer, callback);
+            }
+          }, function (err, results) {
+            // results is now equals to: {one: 1, two: 2}
+            //console.log("results", results);
+            outItem.eventType = results.eventTypeID;
+            if (results.performers.hasFavorites) {
+              outItem.favorite = true;
+            }
+            outItem.location = results.location;
+            outItem.performer = results.performers.idsList;
+            out.push(outItem);
+            callback(null)
+          });
+          // arg1 now equals 'one' and arg2 now equals 'two'
+          //callback(null, 'three');
+        },
+        function (callback) {
+
+          outItem.name = item.name;
+          outItem.name_lower = item.name.toLowerCase();
+          outItem.url = item.url;
+          outItem.startDate = item.startDate;
+          outItem.endDate = item.endDate;
+          outItem.website = websiteID;
+          outItem.price = null;
+          outItem.eventImage = item.eventImage;
+          outItem.active = true;
+          console.log("Create: ", outItem.name);
+
+          show_by_date_event.create(outItem)
+            .then(function (res) {
+              callback(null, 'done');
+            }, function (err) {
+              console.log('error:', err);
+              callback(null, 'done');
+              // handle error here.
+            })
+          // arg1 now equals 'three')
+
+        }
+      ], function (err, result) {
+        if (err) {
+          next();
+        } else {
+          out.push(outItem);
+          next();
+        }
+      });
+    },
+    function (err) {
+      // console.log(out);
+      //return res.status(200).json(out)
+      console.log(err);
+      if (cb) {
+        cb();
+      }
+      ;
+    })
 }
